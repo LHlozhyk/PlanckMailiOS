bd7ee624b321a3043d6df92892220acc5296ef66
lyupko <lyubomyr.hlozhyk@gmail.com>
2015-05-10 00:22:22 +0300

    Created project

---
+++ planckMailiOS/planckMailiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  planckMailiOS
+//
+//  Created by Admin on 5/10/15.
+//  Copyright (c) 2015 LHlozhyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ planckMailiOS/planckMailiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  planckMailiOS
+//
+//  Created by Admin on 5/10/15.
+//  Copyright (c) 2015 LHlozhyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
